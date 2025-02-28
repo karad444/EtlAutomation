9caa155c1a083fd8831074295328ae187f339bb8
sagar4693 <karads568>
2025-02-28 14:16:32 +0530

    ,file done

---
+++ add.py
@@ -0,0 +1,9 @@
+import time
+
+from selenium import webdriver
+
+driver=webdriver.Chrome()
+driver.maximize_window()
+time.sleep(5)
+driver.get("https://www.amazon.in/?&tag=googhydrabk1-21&ref=pd_sl_7hz2t19t5c_e&adgrpid=155259815513&hvpone=&hvptwo=&hvadid=676742245123&hvpos=&hvnetw=g&hvrand=6214736145677537882&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9301536&hvtargid=kwd-10573980&hydadcr=14453_2367553")
+time.sleep(5)
